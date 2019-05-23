5db57209c81d62e770388a12e578a4c0ea047b03
soleimani1984 <z.soleimani1984@gmail.com>
2019-05-23 22:42:31 +0200

    warmup done

--- javascript/javascript2/week2/warmup.js
+++ javascript/javascript2/week2/warmup.js
@@ -2,18 +2,51 @@
 let numbers = [1, 2, 3, 4];
 let newNumbers = [];
 let oddNumbers = numbers.filter(number => number % 2 !==0);
-newNumbers = oddNumbers.map(number => number * 2);
+oddDoubling = oddNumbers.map(number => number * 2);
 
 console.log(oddNumbers);
-console.log("The doubled numbers are", newNumbers);
+console.log(oddDoubling);
 
 //warmup movie
-//movies with a short title 
+//1. short movie titles 
 let shortTitles = movies.filter(movie => movie.title.length <= 6);
 console.log(shortTitles);
 
-//long movie titles
+//2. long movie titles
 let longTitles = movies.filter(movie => movie.title.length > 60);
 console.log(longTitles);
 
+//3. number of movies
+let yearOfMovies = movies.filter(movie => movie.year >=1980 && movie.year <=1989);
+console.log(yearOfMovies);
 
+//4. new array
+let movieRating = movies.map(movie => {
+    if (movie.rating >= 7) {
+       movie.tag = "Good"; 
+    }
+    else if (movie.rating >= 4 && movie.rating < 7) {
+        movie.tag = "Average";
+    }
+    else if (movie.rating < 4){
+        movie.tag = "Bad";
+    }
+    return movie;
+});
+console.log(movieRating);
+
+//5. changing
+let highRate = movies
+    .filter(movie => movie.rating > 6)
+    .map(movie => movie.rating);
+
+console.log(highRate);
+
+//6. Count the total number of movies
+let findName = movies.filter(function(movie) {
+    if (movie.title.toLowerCase().includes("surfer") || 
+        movie.title.toLowerCase().includes("alien") ||
+        movie.title.toLowerCase().includes("benjamin"))
+    return movie.title;         
+});
+console.log(findName.length);
