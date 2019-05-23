5b0802ea7e65e28e923176f82ed680019f5cba20
soleimani1984 <z.soleimani1984@gmail.com>
2019-05-23 19:25:29 +0200

    warmup movie added

--- javascript/javascript2/week2/warmup.js
+++ javascript/javascript2/week2/warmup.js
@@ -1,10 +1,19 @@
-//warmup array exercises
+//warmup doubling of numbers:
 let numbers = [1, 2, 3, 4];
 let newNumbers = [];
-
 let oddNumbers = numbers.filter(number => number % 2 !==0);
-let doubleOddNumbers = oddNumbers.map(number => number * 2);
+newNumbers = oddNumbers.map(number => number * 2);
 
 console.log(oddNumbers);
-console.log("The doubled numbers are", doubleOddNumbers);
+console.log("The doubled numbers are", newNumbers);
+
+//warmup movie
+//movies with a short title 
+let shortTitles = movies.filter(movie => movie.title.length <= 6);
+console.log(shortTitles);
+
+//long movie titles
+let longTitles = movies.filter(movie => movie.title.length > 60);
+console.log(longTitles);
+
 
