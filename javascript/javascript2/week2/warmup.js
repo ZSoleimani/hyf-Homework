d0a409dfc8f7d1013387dd1ed6d91f7bc25a40c3
soleimani1984 <z.soleimani1984@gmail.com>
2019-05-23 18:26:57 +0200

    warmup 1.

--- javascript/javascript2/week2/warmup.js
+++ javascript/javascript2/week2/warmup.js
@@ -1,5 +1,10 @@
-//warmup part 1
-setTimeout(function(){ 
-    console.log("called after 2.5 second")
-}, 2500);
+//warmup array exercises
+let numbers = [1, 2, 3, 4];
+let newNumbers = [];
+
+let oddNumbers = numbers.filter(number => number % 2 !==0);
+let doubleOddNumbers = oddNumbers.map(number => number * 2);
+
+console.log(oddNumbers);
+console.log("The doubled numbers are", doubleOddNumbers);
 
